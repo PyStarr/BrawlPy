6835c490de0891d74b25d86ef7bcf6630c9b0bf3
YaSh_ <yashverma30115@gmail.com>
2022-01-20 18:03:58 +0530

    First ever commit :)

--- brawlpy/__init__.py
+++ brawlpy/__init__.py
@@ -2,12 +2,12 @@
 Brawl Stars API Wrapper
 ~~~~~~~~~~~~~~~~~~~~~~~~~
 A basic wrapper for the Brawl Stars API.
-:copyright: (c) 2022-Present BrawlPy Org
+:copyright: (c) 2022-Present PyStarr
 :license: MIT, see LICENSE for more details.
 """
 
 __title__ = 'brawlpy'
-__author__ = 'BrawlPy Org'
+__author__ = 'PyStarr'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2022-Present BrawlPy Org'
+__copyright__ = 'Copyright 2022-Present PyStarr'
 __version__ = '0.0.1'
