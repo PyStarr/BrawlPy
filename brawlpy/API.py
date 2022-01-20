11dd31c753150cd6bdfdddcdd87c41c848ccf9c8
YaSh_ <yashverma30115@gmail.com>
2022-01-20 18:59:01 +0530

    Added CheckTAg

--- brawlpy/API.py
+++ brawlpy/API.py
@@ -9,5 +9,17 @@ class API:
         self.RANKINGS = self.BASE + "/rankings"
         self.BRAWLERS = self.BASE + "/brawlers"
 
-def convertTag(tag : str):
+def checkTag(tag : str):
     tag = tag.strip("#").upper()
+    allowed_chars = "0289PYLQGRJCUV"
+
+    if len(tag) < 3:
+        raise NotFoundError(404, reason="Tag can't be less then 3 characters!")
+    invalid = [i for i in tag if i not in allowed_chars]
+    if invalid:
+        raise NotFoundError(404, reason="An Invalid character has been passed!",invalid_characters=invalid)
+
+    if not tag.startswith("%23"):
+        tag = "%23" + tag
+
+    return tag
