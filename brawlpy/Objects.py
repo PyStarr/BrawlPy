960c72c5a337b28aab5442fba2f40a915a2a36ef
YaSh_ <yashverma30115@gmail.com>
2022-01-21 21:38:00 +0530

    club,player and clubMember done

--- brawlpy/Objects.py
+++ brawlpy/Objects.py
@@ -1,6 +1,6 @@
 
 class Player:
-    def __init__(self,name,tag,nameColor,icon_id,trophies,expLevel,expPoints,club,highestTrophies,soloVictories,duoVictories,teamVictores,bestRoboRumbleTime,bestTimeAsBigBrawler,brawlers):
+    def __init__(self,name,tag,nameColor,icon_id,trophies,expLevel,expPoints,club,highestTrophies,soloVictories,duoVictories,teamVictories,bestRoboRumbleTime,bestTimeAsBigBrawler,brawlers):
         self.name = name
         self.tag = tag
         self.nameColor = nameColor
@@ -11,7 +11,7 @@ class Player:
         self.highestTrophies = highestTrophies
         self.soloVictories = soloVictories
         self.duoVictories = duoVictories
-        self.teamVictores = teamVictores
+        self.teamVictories = teamVictories
         self.bestTimeAsBigBrawler = bestTimeAsBigBrawler
         self.bestRoboRumbleTime = bestRoboRumbleTime
         self.brawlers = brawlers
@@ -40,6 +40,21 @@ class Club:
     def __str__(self):
         return "{0.name} ({0.tag})".format(self)
 
+class ClubMember:
+    def __init__(self,name,icon_id,tag,role,nameColor):
+        self.name = name
+        self.icon_id = icon_id
+        self.tag = tag
+        self.role = role
+        self.nameColor = nameColor
+    
+    def __repr__(self):
+        return "<ClubMember name='{0.name}' tag='{0.tag}' role='{0.role}'>".format(self)
+
+    def __str__(self):
+        return "{0.name} ({0.tag}) => {0.role}".format(self)
+
+
 class ClubRanking:
     def __init__(self,tag,name,badgeID,trophies,rank,memberCount):
         self.tag = tag
@@ -66,7 +81,103 @@ class PlayerRanking:
         self.clubName = clubName
 
     def __repr__(self):
-        return "<Player name='{0.name}' tag='{0.tag}' rank={0.rank}>".format(self)
+        return "<Club name='{0.name}' tag='{0.tag}'>".format(self)
 
     def __str__(self):
-        return "{0.rank}. {0.name} ({0.tag})".format(self)
+        return "{0.name} ({0.tag})".format(self)
+
+
+class BrawlerRanking:
+    def __init__(self,brawler,playerTag,playerName,playerNameColor,playerIconID,rank,clubName):
+        self.brawler = brawler
+        self.playerTag = playerTag
+        self.playerName = playerName
+        self.playerNameColor = playerNameColor
+        self.playerIconID = playerIconID
+        self.rank = rank
+        self.clubName = clubName
+
+    def __repr__(self):
+        return "<Player name='{0.name}' tag='{0.tag}' rank={0.rank} brawler={0.brawler}>".format(self)
+
+    def __str__(self):
+        return "{0.rank}. {0.name} ({0.tag}) => {0.brawler}".format(self)
+
+class Event:
+    def __init__(self,id,mode,map,startTime,endTime):
+        self.id = id
+        self.mode = mode
+        self.map = map
+        self.startTime = startTime
+        self.endTime = endTime
+    
+    def __repr__(self):
+        return "<Event mode='{0.mode}' map='{0.map}' id={0.id}>".format(self)
+
+    def __str__(self):
+        return "{0.mode} => {0.map} ({0.id})".format(self)
+
+class Brawler:
+    def __init__(self,name,id,starPowers,gadgets):
+        self.name = name
+        self.id = id
+        self.starPowers = starPowers
+        self.gadgets = gadgets
+    
+    def __repr__(self):
+        return "<Brawler name='{0.name}' id='{0.id}'>".format(self)
+
+    def __str__(self):
+        return "{0.name} ({0.id})".format(self)
+
+class PlayerBrawler:
+    def __init__(self,name,id,power,rank,highestTrophies,gadgets,gears,starPowers):
+        self.name = name
+        self.id = id
+        self.power = power
+        self.rank = rank
+        self.highestTrophies = highestTrophies
+        self.gadgets = gadgets
+        self.gears = gears
+        self.starPowers = starPowers
+    
+    def __repr__(self):
+        return "<Brawler name='{0.name}' playerName='{0.player.name}' trophies={0.trophies}>".format(self)
+
+    def __str__(self):
+        return "{0.name} ({0.player})".format(self)
+
+
+class Gadget:
+    def __init__(self,name,id):
+        self.name = name
+        self.id = id
+    
+    def __repr__(self):
+        return "<Gadget name='{0.name}' id='{0.id}'>".format(self)
+
+    def __str__(self):
+        return "{0.name} ({0.id})".format(self)
+
+class StarPower:
+    def __init__(self,name,id):
+        self.name = name
+        self.id = id
+    
+    def __repr__(self):
+        return "<StarPower name='{0.name}' id='{0.id}'>".format(self)
+
+    def __str__(self):
+        return "{0.name} ({0.id})".format(self)
+
+class Gear:
+    def __init__(self,name,id,level):
+        self.name = name
+        self.id = id
+        self.level = level
+    
+    def __repr__(self):
+        return "<Gear name='{0.name}' id='{0.id}' level={0.level}>".format(self)
+    
+    def __str__(self):
+        return "level {0.level} gear {0.name} ({0.id})".format(self)
--- brawlpy/main.py
+++ brawlpy/main.py
@@ -1,3 +1,4 @@
+import imp
 from .API import API, checkTag
 from .errors import *
 from .utils import *
@@ -5,6 +6,7 @@ from .utils import *
 import aiohttp
 import asyncio
 import json
+from .Objects import *
 
 class Client:
     def __init__(self,token):
@@ -42,17 +44,54 @@ class Client:
         Tag = checkTag(tag)
 
         player = await self.request(self.api.PLAYER.format(playerTag=Tag))
+        c = player['club']
+        if len(c) < 1:
+            cl = None
+        else:
+            cc = await self.get_club(c['tag'])
+            cc_members = []
+            for each in cc['members']:
+                m = ClubMember(each['name'],each['icon']['id'],each['tag'],each['role'],each['nameColor'])
+                cc_members.append(m)
+            cl = Club(cc['tag'],cc['name'],cc['description'],cc['type'],cc['badgeId'],cc['requiredTrophies'],cc['trophies'],cc_members)
+        
+        brs = player['brawlers']
+        brrs = []
+        for each in brs:
+            grs = []
+            srs = []
+            gears = []
+            for i in each['gadgets']:
+                gr = Gadget(i['name'],i['id'])
+                grs.append(gr)
+            for i in each['starPowers']:
+                sr = StarPower(i['name'],i['id'])
+                srs.append(sr)
+            for i in each['gears']:
+                gear = Gear(i['name'],i['id'],i['level'])
+                gears.append(gear)
 
-        return player
+            br = PlayerBrawler(each['name'],each['id'],each['power'],each['rank'],each['highestTrophies'],grs,gears,srs)
+            brrs.append(br)
+
+        Pl = Player(player['name'],player['tag'],player['nameColor'],player['icon']['id'],player['trophies'],player['expLevel'],player['expPoints'],cl,player['highestTrophies'],player['soloVictories'],player['duoVictories'],player['3vs3Victories'],player['bestRoboRumbleTime'],player['bestTimeAsBigBrawler'],brrs)
+
+        return Pl
 
     async def get_club(self,tag):
         """Get a club by tag"""
 
         Tag = checkTag(tag)
 
-        club = await self.reason(self.api.CLUB.format(clubTag=Tag))
+        club = await self.request(self.api.CLUB.format(clubTag=Tag))
 
-        return club
+        cc_members = []
+        for each in club['members']:
+            m = ClubMember(each['name'],each['icon']['id'],each['tag'],each['role'],each['nameColor'])
+            cc_members.append(m)
+        cl = Club(club['tag'],club['name'],club['description'],club['type'],club['badgeId'],club['requiredTrophies'],club['trophies'],cc_members)
+
+        return cl
 
     async def brawlers(self):
         """Get a list of all the brawlers currently in the game"""
