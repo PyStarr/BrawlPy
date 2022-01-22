acc60df71a007ddbaa6f63d1dde6bb060076303d
YaSh_ <yashverma30115@gmail.com>
2022-01-22 17:37:56 +0530

    moved checkTag to utls

--- brawlpy/API.py
+++ brawlpy/API.py
@@ -10,18 +10,3 @@ class API:
         self.BRAWLERS = self.BASE + "/brawlers"
         self.BRAWLER = self.BASE + "/brawlers/{id}"
         self.EVENTS = self.BASE + "/events/rotation"
-
-def checkTag(tag : str):
-    tag = tag.strip("#").upper()
-    allowed_chars = "0289PYLQGRJCUV"
-
-    if len(tag) < 3:
-        raise TagNotFoundError(404, reason="Tag can't be less then 3 characters!")
-    invalid = [i for i in tag if i not in allowed_chars]
-    if invalid:
-        raise TagNotFoundError(404, reason="An Invalid character has been passed!",invalid_characters=invalid)
-
-    if not tag.startswith("%23"):
-        tag = "%23" + tag
-
-    return tag
