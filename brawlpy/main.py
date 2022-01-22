51aed3757a914a08b5a1f9257062cbd96195af22
YaSh_ <yashverma30115@gmail.com>
2022-01-22 16:53:31 +0530

    Switched all the dicts to objects

--- brawlpy/main.py
+++ brawlpy/main.py
@@ -1,4 +1,8 @@
 import imp
+from xml.dom import NotFoundErr
+
+from discord import NotFound
+from itsdangerous import exc
 from .API import API, checkTag
 from .errors import *
 from .utils import *
@@ -24,136 +28,299 @@ class Client:
         async with self.session.get(url=url) as resp:
             status = resp.status
             data = await resp.json()
-
-            if 300 > status >= 200:
-                return data
-            elif status == 403:
-                raise Forbidden(status, url, data['message'])
-            elif status == 404:
-                raise NotFoundError(status, reason='Not Found!')
-            elif status == 429:
-                raise RateLimitError(status, url)
-            elif status == 500:
-                raise UnexpectedError(status, url, data)
-            elif status == 503:
-                raise ServerError(status, url)
+            
+            return data, status
+            
+            
 
     async def get_player(self,tag):
         """Get a player by tag"""
 
         Tag = checkTag(tag)
-
-        player = await self.request(self.api.PLAYER.format(playerTag=Tag))
-        c = player['club']
-        if len(c) < 1:
-            cl = None
-        else:
-            cc = await self.get_club(c['tag'])
-            cc_members = []
-            for each in cc['members']:
-                m = ClubMember(each['name'],each['icon']['id'],each['tag'],each['role'],each['nameColor'])
-                cc_members.append(m)
-            cl = Club(cc['tag'],cc['name'],cc['description'],cc['type'],cc['badgeId'],cc['requiredTrophies'],cc['trophies'],cc_members)
         
-        brs = player['brawlers']
-        brrs = []
-        for each in brs:
-            grs = []
-            srs = []
-            gears = []
-            for i in each['gadgets']:
-                gr = Gadget(i['name'],i['id'])
-                grs.append(gr)
-            for i in each['starPowers']:
-                sr = StarPower(i['name'],i['id'])
-                srs.append(sr)
-            for i in each['gears']:
-                gear = Gear(i['name'],i['id'],i['level'])
-                gears.append(gear)
+        url = self.api.PLAYER.format(playerTag=Tag)
 
-            br = PlayerBrawler(each['name'],each['id'],each['power'],each['rank'],each['trophies'],each['highestTrophies'],grs,gears,srs)
-            brrs.append(br)
+        player, status = await self.request(url)
+        if 300 > status >= 200:
+            c = player['club']
+            if len(c) < 1:
+                cl = None
+            else:
+                cc = await self.get_club(c['tag'])
+                cc_members = []
+                for each in cc['members']:
+                    m = ClubMember(each['name'],each['icon']['id'],each['tag'],each['role'],each['nameColor'])
+                    cc_members.append(m)
+                cl = Club(cc['tag'],cc['name'],cc['description'],cc['type'],cc['badgeId'],cc['requiredTrophies'],cc['trophies'],cc_members)
+                
+            brs = player['brawlers']
+            brrs = []
+            for each in brs:
+                grs = []
+                srs = []
+                gears = []
+                for i in each['gadgets']:
+                    gr = Gadget(i['name'],i['id'])
+                    grs.append(gr)
+                for i in each['starPowers']:
+                    sr = StarPower(i['name'],i['id'])
+                    srs.append(sr)
+                for i in each['gears']:
+                    gear = Gear(i['name'],i['id'],i['level'])
+                    gears.append(gear)
 
-        Pl = Player(player['name'],player['tag'],player['nameColor'],player['icon']['id'],player['trophies'],player['expLevel'],player['expPoints'],cl,player['highestTrophies'],player['soloVictories'],player['duoVictories'],player['3vs3Victories'],player['bestRoboRumbleTime'],player['bestTimeAsBigBrawler'],brrs)
+                br = PlayerBrawler(each['name'],each['id'],each['power'],each['rank'],each['trophies'],each['highestTrophies'],grs,gears,srs)
+                brrs.append(br)
 
-        return Pl
+            Pl = Player(player['name'],player['tag'],player['nameColor'],player['icon']['id'],player['trophies'],player['expLevel'],player['expPoints'],cl,player['highestTrophies'],player['soloVictories'],player['duoVictories'],player['3vs3Victories'],player['bestRoboRumbleTime'],player['bestTimeAsBigBrawler'],brrs)
+
+            return Pl
+
+        elif status == 403:
+            raise Forbidden(status, url, url['message'])
+        elif status == 404:
+            raise TagNotFoundError(status)
+        elif status == 429:
+            raise RateLimitError(status, url)
+        elif status == 500:
+            raise UnexpectedError(status, url)
+        elif status == 503:
+            raise ServerError(status, url)
 
     async def get_club(self,tag):
         """Get a club by tag"""
 
         Tag = checkTag(tag)
+        url = self.api.CLUB.format(clubTag=Tag)
+        club , status= await self.request(url)
 
-        club = await self.request(self.api.CLUB.format(clubTag=Tag))
+        if 300 > status >= 200:
+            cc_members = []
+            for each in club['members']:
+                m = ClubMember(each['name'],each['icon']['id'],each['tag'],each['role'],each['nameColor'])
+                cc_members.append(m)
+            cl = Club(club['tag'],club['name'],club['description'],club['type'],club['badgeId'],club['requiredTrophies'],club['trophies'],cc_members)
 
-        cc_members = []
-        for each in club['members']:
-            m = ClubMember(each['name'],each['icon']['id'],each['tag'],each['role'],each['nameColor'])
-            cc_members.append(m)
-        cl = Club(club['tag'],club['name'],club['description'],club['type'],club['badgeId'],club['requiredTrophies'],club['trophies'],cc_members)
-
-        return cl
+            return cl
+        
+        elif status == 403:
+            raise Forbidden(status, url, club['message'])
+        elif status == 404:
+            raise TagNotFoundError(status)
+        elif status == 429:
+            raise RateLimitError(status, url)
+        elif status == 500:
+            raise UnexpectedError(status, url, club)
+        elif status == 503:
+            raise ServerError(status, url)
 
     async def brawlers(self):
         """Get a list of all the brawlers currently in the game"""
         
-        brawlers = await self.request(self.api.BRAWLERS)
+        url = self.api.BRAWLERS
+        brawlers, status = await self.request(url)
+        if 300 > status >= 200:
+            brss = brawlers[0]['items']
+            brs = []
+            for b in brss:
+                gadgets = []
+                srs = []
+                
+                for each in b['gadgets']:
+                    gr = Gadget(each['name'],each['id'])
+                    gadgets.append(gr)
+                
+                for each in b['starPowers']:
+                    sr = StarPower(each['name'],each['id'])
+                    srs.append(sr)
 
-        brss = brawlers['items']
-        gadgets = []
-        srs = []
-        
-        for each in brss['gadgets']:
-            gr = Gadget(each['name'],each['id'])
-            gadgets.append(gr)
-        
-        for each in brss['starPowers']:
-            sr = StarPower(each['name'],each['id'])
-            srs.append(sr)
+                brs.append(Brawler(b['name'],b['id'],srs,gadgets))
 
-        Brs = Brawler(brawlers['name'],brawlers['id'],srs,gadgets)
-
-        return Brs
+            return brs
+        elif status == 403:
+            raise Forbidden(status, url, url['message'])
+        elif status == 404:
+            raise BrawlerNotFound(status)
+        elif status == 429:
+            raise RateLimitError(status, url)
+        elif status == 500:
+            raise UnexpectedError(status, url)
+        elif status == 503:
+            raise ServerError(status, url)
 
     async def events(self):
         """Get the list of all the events currently in rotation"""
         
-        events = await self.request(self.api.EVENTS)
+        url = self.api.EVENTS
 
-        return events
+        events, status = await self.request(url)
 
-    async def get_club_rankings(self,countryCode='global'):
-        """Get top club rankings"""
+        if 300 > status >= 200:
+            Ev = Event(events['id'],events['mode'],events['map'],events['startTime'],events['endTime'])
 
-        rankings = await self.request(self.api.RANKINGS.format(countryCode=countryCode) + "/clubs")
-
-        return rankings
+            return Ev
+        
+        elif status == 403:
+            raise Forbidden(status, url, url['message'])
+        elif status == 404:
+            raise NotFoundErr(status, "Not Found!")
+        elif status == 429:
+            raise RateLimitError(status, url)
+        elif status == 500:
+            raise UnexpectedError(status, url)
+        elif status == 503:
+            raise ServerError(status, url)
 
     async def get_players_rankings(self,countryCode='global'):
         """Get top players rankings"""
 
-        rankings = await self.request(self.api.RANKINGS.format(countryCode=countryCode) + "/players")
+        url = self.api.RANKINGS.format(countryCode=countryCode) + "/players"
 
-        return rankings
+        rankings, status = await self.request(url)
+
+        if 300 > status >= 200:
+        
+            rankgs = []
+
+            for each in rankings['items']:
+                try:
+                    cl = each['club']
+                except KeyError:
+                    cl = None
+                else:
+                    cl = cl['name']
+                rankgs.append(PlayerRanking(each['name'],each['tag'],each['nameColor'],each['icon']['id'],each['trophies'],each['rank'],cl))
+
+            return rankgs
+
+        elif status == 403:
+            raise Forbidden(status, url, url['message'])
+        elif status == 404:
+            raise NotFoundErr(status, "Not Found!")
+        elif status == 429:
+            raise RateLimitError(status, url)
+        elif status == 500:
+            raise UnexpectedError(status, url)
+        elif status == 503:
+            raise ServerError(status, url)
 
     async def get_brawlers_rankings(self,brawlerID,countryCode='global'):
         """Get top rankings based on brawlers"""
 
-        brawler = (getBrawlerbyID(await self.brawlers(),brawlerID))[0]
+        url = self.api.RANKINGS.format(countryCode=countryCode) + "/brawlers/{}".format(brawlerID)
 
-        rankings = await self.request(self.api.RANKINGS.format(countryCode=countryCode) + "/brawlers/{}".format(brawler['id']))
+        rankings, status = await self.request(url)
+        
+        if 300 > status >= 200:
+            rankgs = []
 
-        return rankings
+            for each in rankings['items']:
+                try:
+                    cl = each['club']
+                except KeyError:
+                    cl = None
+                else:
+                    cl = cl['name']
+                rankgs.append(BrawlerRanking(each['tag'],each['name'],each['nameColor'],each['icon']['id'],each['trophies'],each['rank'],cl))
+            
+            return rankgs
+        
+        elif status == 403:
+            raise Forbidden(status, url, url['message'])
+        elif status == 404:
+            raise BrawlerNotFound(status, brawlerID)
+        elif status == 429:
+            raise RateLimitError(status, url)
+        elif status == 500:
+            raise UnexpectedError(status, url)
+        elif status == 503:
+            raise ServerError(status, url)
 
     async def get_club_rankings(self,countryCode='global'):
         """Get top clubs rankings"""
 
-        rankings = await self.request(self.api.RANKINGS.format(countryCode=countryCode) + "/clubs")
+        url = self.api.RANKINGS.format(countryCode=countryCode) + "/clubs"
 
-        return rankings
+        rankings, status = await self.request(url)
+
+        if 300 > status >= 200:
+            clubs = []
+
+            for each in rankings['items']:
+                clubs.append(ClubRanking(each['tag'],each['name'],each['badgeId'],each['trophies'],each['rank'],each['memberCount']))
+
+            return clubs
+        
+        elif status == 403:
+            raise Forbidden(status, url, url['message'])
+        elif status == 404:
+            raise CountryNotFound(status, countryCode)
+        elif status == 429:
+            raise RateLimitError(status, url)
+        elif status == 500:
+            raise UnexpectedError(status, url)
+        elif status == 503:
+            raise ServerError(status, url)
 
     async def get_club_members(self, clubTag):
         """Get members of a club"""
+        
+        tag = checkTag(clubTag)
 
-        club = await self.request(self.api.CLUB.format(tag=clubTag) + '/members')
+        url = self.api.CLUB.format(clubTag=tag) + '/members'
 
-        return club
+        club, status = await self.request(url)
+
+        if 300 > status >= 200:
+            cc_members = []
+            for each in club['items']:
+                m = ClubMember(each['name'],each['icon']['id'],each['tag'],each['role'],each['nameColor'],each['trophies'])
+                cc_members.append(m)
+
+            return cc_members
+        
+        elif status == 403:
+            raise Forbidden(status, url, url['message'])
+        elif status == 404:
+            raise TagNotFoundError(status)
+        elif status == 429:
+            raise RateLimitError(status, url)
+        elif status == 500:
+            raise UnexpectedError(status, url)
+        elif status == 503:
+            raise ServerError(status, url)
+    
+    async def get_brawler(self, brawlerID : int):
+        """Get A brawler by id"""
+        
+        url = self.api.BRAWLER.format(id=brawlerID)
+
+        b, status = await self.request(url)
+        
+        if 300 > status >= 200:
+            gadgets = []
+            srs = []
+            
+            for each in b['gadgets']:
+                gr = Gadget(each['name'],each['id'])
+                gadgets.append(gr)
+            
+            for each in b['starPowers']:
+                sr = StarPower(each['name'],each['id'])
+                srs.append(sr)
+
+            brawl = await self.get_brawler(brawlerID)
+
+            br = Brawler(brawl,b['name'],b['id'],srs,gadgets)
+
+            return br
+        elif status == 403:
+            raise Forbidden(status, url, url['message'])
+        elif status == 404:
+            raise BrawlerNotFound(status, id = brawlerID)
+        elif status == 429:
+            raise RateLimitError(status, url)
+        elif status == 500:
+            raise UnexpectedError(status, url, b)
+        elif status == 503:
+            raise ServerError(status, url)
